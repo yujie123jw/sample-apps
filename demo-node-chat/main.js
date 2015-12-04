c3309aa6e0dfc8d0c44b9cc482e2c2c1fcb67414
Phillip Tribble <phillip.tribble@gmail.com>
2015-12-03 22:25:02 -0800

    Beta Twitter Support

--- demo-node-chat/main.js
+++ demo-node-chat/main.js
@@ -9,6 +9,19 @@ var username = process.env.USERNAME;
 var password = process.env.PASSWORD;
 var port = process.env.PORT;
 var database = 'chat';
+var Twitter = require('twitter');
+var consumer_key = process.env.consumer_key;
+var consumer_secret = process.env.consumer_secret;
+var access_token_key = process.env.access_token_key;
+var access_token_secret = process.env.access_token_secret;
+var twitter_topic = process.env.twitter_topic;
+
+var client = new Twitter({
+ "consumer_key": consumer_key,
+ "consumer_secret": consumer_secret,
+ "access_token_key": access_token_key,
+ "access_token_secret": access_token_secret
+});
 
 if(job_bind){
 job_bind = job_bind.replace("tcp://","mysql://" + username + ':' + password + '@');
@@ -24,6 +37,28 @@ var uri = mysql_bind.replace(parse_database[3], "");
 var connection = mysql.createPool(mysql_bind);
 var connection_create = mysql.createPool(uri);
 createtable(createtable_async);	
+}
+
+function look_for_tweets() {
+setTimeout(function () {
+   	client.stream('statuses/filter', {track: twitter_topic}, function(stream) {
+   		stream.on('data', function(tweet) {
+   			console.log('\n[From Twitter about ' + twitter_topic + '] ' tweet.text);
+   			sendMessage('From Twitter: ' + tweet.text);
+   			showAll(showall_async); 
+  		});
+  		stream.on('error', function(error) {
+    		console.log(error);
+    	});
+	});
+    look_for_tweets();
+}, 60000);
+}
+
+if(consumer_key && consumer_secret && access_token_key && access_token_secret && twitter_topic){
+look_for_tweets();
+} else {
+console.log('\n[Twitter Support disabled]\n');	
 }
 
 function showAll (callback) {
@@ -44,61 +79,61 @@ function showAll (callback) {
 
 
 function truncate () {
-     data = "";
+    data = "";
     connection.getConnection(function(err,connection) {
-	connection.query("truncate chat;", function(err, rows) {
-	connection.release();
+		connection.query("truncate chat;", function(err, rows) {
+			connection.release();
         });
-        });
+     });
 }
 
 function showall_async(data){
-			data.reverse();
-			io.emit('show all', 'clear');
-			for(var i=0;i != data.length;i++){
-			io.emit('show all', data[i].item);
-			}
+	data.reverse();
+	io.emit('show all', 'clear');
+		for(var i=0;i != data.length;i++){
+		io.emit('show all', data[i].item);
+		}
 }
 
 function sendMessage(mdata) {
 	var chat_data = mdata;
 	connection.getConnection(function(err,connection) {
-	if(mdata.indexOf('\'') != -1){
-	chat_data = mdata.replace(/\'/g,"\\\'");
-	}
-
-	connection.query("insert into chat (item) values('" + chat_data +"');", function(err,rows) {
-		if(err) {
-			console.log('Error sending chat data', err);
+		if(mdata.indexOf('\'') != -1){
+			chat_data = mdata.replace(/\'/g,"\\\'");
 		}
-	connection.release();
-	});
+
+		connection.query("insert into chat (item) values('" + chat_data +"');", function(err,rows) {
+			if(err) {
+				console.log('Error sending chat data', err);
+			}
+		connection.release();
+		});
 	});
 }
 
 function createdb(callback){
-connection_create.getConnection(function(err,connection) {
- connection_create.query("create database " + database + ";", function(err, rows) {
-                if(err) {
+	connection_create.getConnection(function(err,connection) {
+		connection_create.query("create database " + database + ";", function(err, rows) {
+    		if(err) {
                 console.log('Error creating database', err);
                 }
-connection.release();
-createtable(createtable_async);
-callback('Creating Database.....');
-                });
-                });
+	connection.release();
+	createtable(createtable_async);
+	callback('Creating Database.....');
+         });
+    });
 }
 
 function createtable(callback){
-  connection.getConnection(function(err, connection){
-  connection.query("CREATE TABLE chat (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,item VARCHAR(100));", function(err, rows) {
+	connection.getConnection(function(err, connection){
+		connection.query("CREATE TABLE chat (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,item VARCHAR(100));", function(err, rows) {
                 if(err) {
                 console.log('Error creating table',err);
                 }
-callback('Creating Table......');
-	connection.release();
-            });
-            });
+		callback('Creating Table......');
+		connection.release();
+         });
+    });
 }
 
 function createdb_async(data) {
