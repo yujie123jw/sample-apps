fe06cf980d2402319ae7ade12824ef47d06f741c
Phillip Tribble <phillip.tribble@gmail.com>
2015-12-07 11:54:26 -0800

    Twitter App

---
+++ demo-node-twitter/README.md
@@ -0,0 +1,28 @@
+# License
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Description 
+Apcera Twitter, is a simple twitter feed program that stores tweets in a MySQL Database. To have Apcera Twitter listen for Tweets, the following environment variables need to be set before the application launches: consumer_key, consumer_secret, access_token_key, access_token_secret, and twitter_topic. If any of these variables are not specified, Twitter  will be disabled. They keys are available from your apps.twitter.com account. The variable "twitter_topic" sets what Apcera Twitter will look for from the Twitter live stream and paste the Tweet into the page.
+
+# Run Apcera Twitter from the Apcera Platform with a service gateway
+```
+apc docker run mysqldatabase -i rusher81572/mysql-dev --port 3306 --batch
+(Wait 30 seconds for database to initialize)
+apc provider register mysqldatabase-provider -j mysqldatabase --u mysql://root:sql@mysqldatabase --batch
+apc app create apceratwitter -ae --batch
+apc service create mysqldatabase-service --provider mysqldatabase-provider -j apceratwitter --batch
+apc app update apceratwitter -e consumer_key="***" -e consumer_secret="***" -e access_token_key="***" -e access_token_secret="***" -e twitter_topic="***"
+apc app start apceratwitter --batch
+```
+
+# Run Apcera Twitter from the Apcera Platform with a job binding
+```
+apc docker run mysqldatabase -i rusher81572/mysql-dev --port 3306
+(Wait 30 seconds for database to initialize)
+apc app create apceratwitter -e USERNAME="root" -e PASSWORD="sql" -ae --batch 
+apc job link apceratwitter -t mysqldatabase -n dblink -p 3306
+apc app update apceratwitter -e consumer_key="***" -e consumer_secret="***" -e access_token_key="***" -e access_token_secret="***" -e twitter_topic="***"
+apc app start apceratwitter
+```
+
+Now you should be able to access it in your web browser with the URL located from "apc app show apceratwitter".
---
+++ demo-node-twitter/main.js
@@ -0,0 +1,180 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var data = [];
+var mysql = require('mysql');
+var job_bind = process.env.DBLINK_URI;
+var mysql_bind = process.env.MYSQL_URI;
+var username = process.env.USERNAME;
+var password = process.env.PASSWORD;
+var port = process.env.PORT;
+var database = 'chat';
+var Twitter = require('twitter');
+var consumer_key = process.env.consumer_key;
+var consumer_secret = process.env.consumer_secret;
+var access_token_key = process.env.access_token_key;
+var access_token_secret = process.env.access_token_secret;
+var twitter_topic = process.env.twitter_topic;
+
+var client = new Twitter({
+ "consumer_key": consumer_key,
+ "consumer_secret": consumer_secret,
+ "access_token_key": access_token_key,
+ "access_token_secret": access_token_secret
+});
+
+if(job_bind){
+job_bind = job_bind.replace("tcp://","mysql://" + username + ':' + password + '@');
+var connection = mysql.createPool(job_bind + '/' + database);
+var connection_create = mysql.createPool(job_bind + '/');
+createdb(createdb_async);
+}
+
+if(mysql_bind){
+var parse_database =  mysql_bind.toString().split("/");
+database = parse_database[3];
+var uri = mysql_bind.replace(parse_database[3], "");
+var connection = mysql.createPool(mysql_bind);
+var connection_create = mysql.createPool(uri);
+createtable(createtable_async);	
+}
+
+if(consumer_key && consumer_secret && access_token_key && access_token_secret && twitter_topic){
+client.stream('statuses/filter', {track: twitter_topic}, function(stream) {
+  stream.on('data', function(tweet) {
+    sendMessage(tweet.text);
+	showAll(showall_async);
+  });
+ 
+  stream.on('error', function(error) {
+   console.log(error)
+  }); 
+});
+} else {
+console.log('\n[Twitter Support disabled]\n');	
+}
+
+function showAll (callback) {
+	data = "";
+	connection.getConnection(function(err,connection) {
+ 	connection.query("select * from chat order by id desc limit 18;", function(err, rows) {	
+		if (!err)  {
+			data = rows;
+		 }else {
+			data =  "An error has occurred.";
+			console.log(err);
+		}
+		connection.release();
+		callback(data);
+		});
+		});
+}
+
+
+function truncate () {
+    data = "";
+    connection.getConnection(function(err,connection) {
+		connection.query("truncate chat;", function(err, rows) {
+			connection.release();
+        });
+     });
+}
+
+function showall_async(data){
+	data.reverse();
+	io.emit('show all', 'clear');
+		for(var i=0;i != data.length;i++){
+		io.emit('show all', data[i].item);
+		}
+}
+
+function sendMessage(mdata) {
+	var chat_data = mdata;
+	connection.getConnection(function(err,connection) {
+		if(mdata.indexOf('\'') != -1){
+			chat_data = mdata.replace(/\'/g,"\\\'");
+		}
+
+		connection.query("insert into chat (item) values('" + chat_data +"');", function(err,rows) {
+			if(err) {
+				console.log('Error sending chat data', err);
+			}
+		connection.release();
+		});
+	});
+}
+
+function createdb(callback){
+	connection_create.getConnection(function(err,connection) {
+		connection_create.query("create database " + database + ";", function(err, rows) {
+    		if(err) {
+                console.log('Error creating database', err);
+                }
+	connection.release();
+	createtable(createtable_async);
+	callback('Creating Database.....');
+         });
+    });
+}
+
+function createtable(callback){
+	connection.getConnection(function(err, connection){
+		connection.query("CREATE TABLE chat (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,item VARCHAR(1000));", function(err, rows) {
+                if(err) {
+                console.log('Error creating table',err);
+                }
+		callback('Creating Table......');
+		connection.release();
+         });
+    });
+}
+
+function createdb_async(data) {
+console.log(data);
+}
+
+function createtable_async(data) {
+console.log(data);
+}
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/logo.png', function(req, res){
+res.sendFile(__dirname + '/logo.png');
+});
+
+app.get('/twitter.png', function(req, res){
+res.sendFile(__dirname + '/twitter.png');
+});
+
+app.get('/socket.io.js', function(req, res){
+res.sendFile(__dirname + '/node_modules/socket.io-client/socket.io.js');
+});
+
+io.on('connection', function(socket){
+socket.on('show all', function(msg){
+showAll(showall_async);
+});
+});
+
+io.on('connection', function(socket){
+socket.on('truncate table', function(msg){
+console.log('Starting to truncate table.........');
+truncate();
+console.log('Finished truncating database.');
+});
+});
+
+io.on('connection', function(socket){
+socket.on('chat message', function(msg){
+//sendMessage(msg);
+showAll(showall_async);
+console.log('chat message: ' + msg);
+});
+});
+
+http.listen(port, function(){
+console.log('listening on *:' + port);
+});
