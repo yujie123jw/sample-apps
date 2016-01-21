aae9ea9272cf5b714f39ca21ba30c73aa05d8c26
Phillip Tribble <phillip.tribble@gmail.com>
2016-01-21 11:40:10 -0800

    Loop now works correctly and app no longer dies

--- example-java-hello/HelloWorld.java
+++ example-java-hello/HelloWorld.java
@@ -1,19 +1,10 @@
 import java.net.*;
 import java.io.*;
 
-
 public class HelloWorld {
-
-    public static void loop() {
-	try {
-	System.out.print("Hello, World");
-	Thread.sleep(15000);
-	loop();
-	} catch (Exception loop) {
-	}
-    }
-
     public static void main(String[] args) throws Exception {
-	loop();
+        System.out.println("Hello, World");
+        Thread.sleep(15000);
+        main(args); 
     }
 }
