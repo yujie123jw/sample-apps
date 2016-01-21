80156029241231e568a00adb048d38f2ca79081e
Phillip Tribble <phillip.tribble@gmail.com>
2016-01-21 10:54:05 -0800

    Loop fix

--- example-java-hello/HelloWorld.java
+++ example-java-hello/HelloWorld.java
@@ -1,10 +1,19 @@
 import java.net.*;
 import java.io.*;
 
+
 public class HelloWorld {
+
+    public static void loop() {
+	try {
+	System.out.print("Hello, World");
+	Thread.sleep(15000);
+	loop();
+	} catch (Exception loop) {
+	}
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World");
-        Thread.sleep(15000);
-	main(null);
+	loop();
     }
 }
